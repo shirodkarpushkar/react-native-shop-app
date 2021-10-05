ba56686f21290a415b26d9a1b4975e03b14930dc
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-05 19:15:15 +0530

    cartitem

---
+++ src/components/shop/cartItem.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import Colors from '../../constants/Colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const CartItem = props => {
+  return (
+    <View style={styles.cartItem}>
+      <View style={styles.itemData}>
+        <Text style={styles.quantity}>{props.quantity} x </Text>
+        <Text style={styles.title}>{props.title}</Text>
+      </View>
+      <View style={styles.itemData}>
+        <Text style={styles.amount}>${props.amount}</Text>
+        <TouchableOpacity onPress={props.onRemove} style={styles.deleteBtn}>
+          <Icon name="md-trash" size={23} color="red" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: 'black',
+    shadowOpacity: 0.6,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    height: 300,
+    margin: 20,
+    overflow: 'hidden',
+  },
+  cartItem: {
+    padding: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  },
+  itemData: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantity: {
+    color: '#888',
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  amount: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  deleteBtn: {
+    marginLeft: 20,
+  },
+  price: {
+    fontSize: 14,
+    color: '#888',
+  },
+  productDetails: {
+    alignItems: 'center',
+    height: '20%',
+    padding: 10,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    height: '20%',
+  },
+  touchable: {
+    overflow: 'hidden',
+  },
+});
+export default CartItem;

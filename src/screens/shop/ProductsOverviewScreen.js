634a3374bd13ea6a71d2d8e3ac21b3538e5d426b
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-05 16:20:40 +0530

    [FEAT] products items

--- src/screens/shop/ProductsOverviewScreen.js
+++ src/screens/shop/ProductsOverviewScreen.js
@@ -1,18 +1,22 @@
 import React, {useState} from 'react';
 import {FlatList, View, Text} from 'react-native';
 import {useSelector} from 'react-redux';
-
+import ProductItem from '../../components/shop/ProductItem';
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
 
   const listItemRender = ({item}) => {
     return (
-      <View>
-        <Text>{item.title}</Text>
-      </View>
+      <ProductItem
+        image={item.imageUrl}
+        title={item.title}
+        price={item.price}
+        onView={() => {}}
+        onAddToCart={() => {}}
+      />
     );
   };
 
   return <FlatList data={products} renderItem={listItemRender} />;
 };
-export default ProductsOverviewScreen
+export default ProductsOverviewScreen;

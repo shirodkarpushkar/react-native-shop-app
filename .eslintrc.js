f804a14c8e90b84c3246bedd7277fa002a3e6c63
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-05 13:34:50 +0530

    [FEAT] init eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,43 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier', 'prettier/standard'],
+  rules: {
+    // Only allow debugger in development
+    'no-debugger': process.env.PRE_COMMIT ? 'warn' : 'off',
+    // Only allow `console.log` in development
+    'no-unused-vars': 0,
+    eqeqeq: 0,
+    'no-console': process.env.PRE_COMMIT
+      ? ['warn', {allow: ['warn', 'error']}]
+      : 'off',
+    'prettier/prettier': 0,
+  },
+  overrides: [
+    {
+      files: ['src/**/*', 'tests/unit/**/*', 'tests/e2e/**/*'],
+      excludedFiles: 'app.config.js',
+      parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+      },
+      env: {
+        browser: true,
+      },
+    },
+    {
+      files: ['**/*.unit.js'],
+      parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+      },
+      env: {jest: true},
+      globals: {
+        mount: false,
+        shallowMount: false,
+        shallowMountView: false,
+        createComponentMocks: false,
+        createModuleStore: false,
+      },
+    },
+  ],
 };

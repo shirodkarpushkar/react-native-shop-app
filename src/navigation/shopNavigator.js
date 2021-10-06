3d039a43dbbb846f65b14232c803b398b7eb0ba2
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-06 11:36:53 +0530

    [FEAT] order drawer navigator

--- src/navigation/shopNavigator.js
+++ src/navigation/shopNavigator.js
@@ -4,8 +4,10 @@ import Colors from '../constants/Colors';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailsScreen from '../screens/shop/ProductDetailsScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const defaultScreenOptions = {
   headerStyle: {
@@ -14,6 +16,7 @@ const defaultScreenOptions = {
   headerTintColor: 'white',
 };
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const ProductsNavigator = () => {
   return (
@@ -30,10 +33,34 @@ const ProductsNavigator = () => {
     </Stack.Navigator>
   );
 };
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      drawerContentOptions={{
+        activeTintColor: Colors.accent,
+      }}>
+      <Drawer.Screen
+        name="Products"
+        component={ProductsNavigator}
+        options={{
+          title: 'All Products',
+        }}
+      />
+      <Drawer.Screen
+        name="OrdersOverview"
+        component={OrdersScreen}
+        options={{
+          title: 'Your Orders',
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
 const Container = () => {
   return (
     <NavigationContainer>
-      <ProductsNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
---
+++ src/screens/shop/OrdersScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {FlatList, View, Text} from 'react-native';
+import { useSelector } from "react-redux";
+
+const OrdersScreen = (props) => {
+    const orders = useSelector((state)=>state.orders.orders)
+    return (<FlatList data={orders} renderItem={({ item}) => (<View><Text>{item.amount}</Text></View>)}/>)
+}
+export default OrdersScreen

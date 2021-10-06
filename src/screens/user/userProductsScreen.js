8d9da0b3bf292c7a2199823a3b3e459f7c3fd37b
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-06 18:44:53 +0530

    [FEAT] delete modal

--- src/screens/user/userProductsScreen.js
+++ src/screens/user/userProductsScreen.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {FlatList, Button, Platform} from 'react-native';
+import {FlatList, Button, Platform, Alert} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 
@@ -14,6 +14,18 @@ const UserProductsScreen = props => {
 
   const editProductHandler = id => {
     props.navigation.navigate('EditProduct', {productId: id});
+  };
+  const deleteHandler = item => {
+    Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+      {text: 'No', style: 'default'},
+      {
+        text: 'Yes',
+        style: 'destructive',
+        onPress: () => {
+          dispatch(productsActions.deleteProduct(item));
+        },
+      },
+    ]);
   };
   useEffect(() => {
     props.navigation.setOptions({
@@ -32,7 +44,7 @@ const UserProductsScreen = props => {
             title="create"
             iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
             onPress={() => {
-              props.navigation.navigate('EditProduct',{});
+              props.navigation.navigate('EditProduct', {});
             }}
           />
         </HeaderButtons>
@@ -62,7 +74,7 @@ const UserProductsScreen = props => {
             color={Colors.primary}
             title="Delete"
             onPress={() => {
-              dispatch(productsActions.deleteProduct(item));
+              deleteHandler(item);
             }}
           />
         </ProductItem>

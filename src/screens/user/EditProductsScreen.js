d0a02cf6918b2a0265acd994425df9846f3addf3
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2021-10-07 10:52:59 +0530

    [FEAT] decimal pad keypad

--- src/screens/user/EditProductsScreen.js
+++ src/screens/user/EditProductsScreen.js
@@ -95,6 +95,7 @@ const EditProductScreen = props => {
               style={styles.input}
               value={price}
               onChangeText={text => setPrice(text)}
+              keyboardType="decimal-pad"
             />
           </View>
         )}
